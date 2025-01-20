2babbbbbf59aa8ae8500934c76d3e5497fca28f8
Sean Frankel <iknowhistory@gmail.com>
2025-01-20 12:26:20 -0500

    Initial commit

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-7_o#j1q_z-u_^!z48f))@6#bg_yyaun6$n*0ip=jhr*_vx=3wi"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
