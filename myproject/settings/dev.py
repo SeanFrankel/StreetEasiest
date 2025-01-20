1763981a9c936f09dca8447e500ce16d44801621
Sean Frankel <iknowhistory@gmail.com>
2025-01-20 16:44:06 -0500

    Initial commit with SQLite database and project files

---
+++ myproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1!y_)5&xbs-s2b-a045lq-k7ce5+ym(#z0!(ag9#h@@)^%c69x"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
