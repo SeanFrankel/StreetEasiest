77a0015892f8823e03a8f16eed5170c0f31d7365
Arthur Frankel <afrankel@solutionstreet.com>
2025-07-13 19:16:58 +0100

    Revert "ok removed a BUNCH of files, and reogranized a ton of random crap"
    This reverts commit 4372c9f4f7796485a75f3798b3f4acc1e5217062.

--- webpack.config.js
+++ webpack.config.js
@@ -6,28 +6,34 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const options = {
     entry: {
-        'main': `./static_src/javascript/main.js`,
+        // multiple entries can be added here
+        // 'main' is ignored from prettier because if vue (or anything else) isn't added
+        // here, it will deem the quotes uneccessary.
+        'main': `./static_src/javascript/main.js`, // prettier-ignore
     },
     resolve: {
         extensions: ['.js'],
     },
     output: {
         path: path.resolve(`./static_compiled/`),
-        filename: 'js/[name].js',
-        clean: true,
+        // based on entry name, e.g. main.js
+        filename: 'js/[name].js', // based on entry name, e.g. main.js
     },
     plugins: [
         new CopyPlugin({
             patterns: [
-                {
-                    from: path.resolve(`./static_src/images`),
-                    to: path.resolve(`./static_compiled/images`),
-                }
+                // Copy images to be referenced directly by Django to the "images" subfolder in static files.
+                // Ignore CSS background images as these are handled separately below
+               {
+                from: path.resolve(`./static_src/images`),
+                to: path.resolve(`./static_compiled/images`),
+               }
             ],
         }),
         new MiniCssExtractPlugin({
             filename: 'css/[name].css',
         }),
+        //  Automatically remove all unused webpack assets on rebuild
         new CleanWebpackPlugin()
     ],
     module: {
@@ -73,11 +79,12 @@ const options = {
                 exclude: /node_modules/,
                 type: 'asset/resource',
                 generator: {
-                    filename: 'fonts/[name][ext]'
+                  filename: 'fonts/[name][ext]'
                 }
-            }
+              }
         ],
     },
+    // externals are loaded via base.html and not included in the webpack bundle.
     externals: {
         // gettext: 'gettext',
     },
@@ -89,27 +96,45 @@ const webpackConfig = (environment, argv) => {
     options.mode = isProduction ? 'production' : 'development';
 
     if (!isProduction) {
+        // https://webpack.js.org/configuration/stats/
         const stats = {
+            // Tells stats whether to add the build date and the build time information.
             builtAt: false,
+            // Add chunk information (setting this to `false` allows for a less verbose output)
             chunks: false,
+            // Add the hash of the compilation
             hash: false,
+            // `webpack --colors` equivalent
             colors: true,
+            // Add information about the reasons why modules are included
             reasons: false,
+            // Add webpack version information
             version: false,
+            // Add built modules information
             modules: false,
+            // Show performance hint when file size exceeds `performance.maxAssetSize`
             performance: false,
+            // Add children information
             children: false,
+            // Add asset Information.
             assets: false,
         };
 
         options.stats = stats;
+
+        // Create JS source maps in the dev mode
+        // See https://webpack.js.org/configuration/devtool/ for more options
         options.devtool = 'inline-source-map';
 
+        // See https://webpack.js.org/configuration/dev-server/.
         options.devServer = {
+            // Enable gzip compression for everything served.
             compress: true,
+            // Shows a full-screen overlay in the browser when there are compiler errors.
             overlay: true,
             clientLogLevel: 'error',
             contentBase: false,
+            // Write compiled files to disk. This makes live-reload work on both port 3000 and 8000.
             writeToDisk: true,
             host: '0.0.0.0',
             allowedHosts: [],
